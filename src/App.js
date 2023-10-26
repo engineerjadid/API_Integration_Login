400c31d8b26889181fca3b7c0369c6c36bb9be99
Nazmos Sakib Jadid <engineerjadid@gmail.com>
2023-10-26 15:15:10 +0600

    Jadid

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import './App.css';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Signin from './Signin';
+import Profile from './Profile';
+
+function App() {
+  const token = localStorage.getItem('accessToken');
+
+  if(!token) {
+    return <Signin />
+  }
+
+  return (
+    <div className="wrapper">
+      <BrowserRouter>
+        <Switch>
+          <Route path="/profile">
+            <Profile />
+          </Route>
+          <Route path="/">
+            <Profile />
+          </Route>
+        </Switch>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
